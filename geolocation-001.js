ac273781239855e5e0ff834e6735d93080fe329e
arturoordorica <66848795+arturoordorica@users.noreply.github.com>
2020-06-12 15:57:40 -0500

    Carga de ejemplos de geolocation.

---
+++ geolocation-001.js
@@ -0,0 +1,40 @@
+﻿var oOpciones = {enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0};
+
+function validarGeolocalizacion()
+{
+	if (navigator.geolocation)
+	{
+		alert("Tu navegador sí soporta los servicios de geolocalización.");
+	}
+	else
+	{
+		alert("Lo siento, tu navegador no soporta los servicios de geolocalización.");
+	}
+}
+
+function abrirMapa()
+{
+	if (navigator.geolocation)
+	{
+		navigator.geolocation.getCurrentPosition(getLocalizacion, controlarErrores, oOpciones);
+	}
+	else
+	{
+		alert("Lo siento, tu navegador no soporta los servicios de geolocalización.");
+	}
+}
+
+function getLocalizacion(oPosicion)
+{
+	var lLongitud = oPosicion.coords.longitude;
+	var lLatitud = oPosicion.coords.latitude;
+
+	window.open(`https://www.openstreetmap.org/#map=5/${lLatitud}/${lLongitud}`);
+}
+
+function controlarErrores(oError)
+{
+	alert("Ocurrió un error al utilizar los servicios de geolocalización: " + oError.message);
+}
